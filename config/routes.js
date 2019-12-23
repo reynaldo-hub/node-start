a1850213bab3839d3b1b71fc61c522bad4039713
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2019-12-22 22:25:39 -0600

    Sentry Integration - Init integration work

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/node';
 import PersonController from '../src/controllers/PersonController.js';
 
 export default (server) => {
@@ -6,4 +7,18 @@ export default (server) => {
 	server.post('/api/persons', PersonController.insert);
 	server.put('/api/persons/:id', PersonController.update);
 	server.delete('/api/persons/:id', PersonController.delete);
+	server.get('/debug-sentry', () => {
+		try {
+			throw new Error('My first Sentry error!');
+		} catch (error) {
+			Sentry.setUser(
+				{
+					email: 'john.wick@example.com',
+					id: 123,
+					username: 'John Wick',
+				},
+			);
+			Sentry.captureException(new Error('Something broke'));
+		}
+	});
 };
--- config/server.js
+++ config/server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import * as Sentry from '@sentry/node';
 import setRoutes from './routes.js';
+
+Sentry.init({
+	dsn: 'https://5847fbaec48242aeb458141074b7eb25@sentry.io/1864371',
+	debug: true,
+});
 
 const server = express();
 
+// The request handler must be the first middleware on the app
+server.use(Sentry.Handlers.requestHandler());
 server.use(bodyParser.json());
 setRoutes(server);
+
 export default server;

8680308e5860c98d1ca025cef8399624a36df1e9
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2020-01-19 18:14:49 -0600

    Authentication - Implemented jwt

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,13 @@
 import PersonController from '../src/controllers/PersonController.js';
+import UserController from '../src/controllers/UserController.js';
+import Auth from '../src/middlewares/jwt.js';
 
 export default (server) => {
-	server.get('/api/persons', PersonController.getAll);
+	server.get('/api/persons', Auth.authorize, PersonController.getAll);
 	server.get('/api/persons/:params', PersonController.get);
 	server.post('/api/persons', PersonController.insert);
 	server.put('/api/persons/:id', PersonController.update);
 	server.delete('/api/persons/:id', PersonController.delete);
+	server.post('/api/users/signup/', UserController.signUp);
+	server.post('/api/users/login/', UserController.login);
 };
